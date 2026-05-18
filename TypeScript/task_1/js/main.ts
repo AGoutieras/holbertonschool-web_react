84a5fe545128e79cb01ce7ea77d5f1a730609b84
AGoutieras <goutieras.anthony@gmail.com>
2026-05-18 16:57:41 +0200

    feat: add StudentClass and related interfaces for student management

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -18,3 +18,32 @@ interface printTeacherFunction {
 function printTeacher(firstName: string, lastName: string): string {
   return firstName.charAt(0) + ". " + lastName;
 }
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
