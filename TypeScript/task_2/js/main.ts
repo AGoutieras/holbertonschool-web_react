dfbcc045ba89c5c636edfd48e0ae1c7c947723ff
AGoutieras <goutieras.anthony@gmail.com>
2026-05-19 11:09:44 +0200

    refactor: clean up code formatting and improve consistency in main.ts

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -1,4 +1,3 @@
-
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
@@ -13,36 +12,58 @@ interface TeacherInterface {
 
 class Director implements DirectorInterface {
   workFromHome(): string {
-      return "Working from home"
+    return "Working from home";
   }
 
   getCoffeeBreak(): string {
-      return "Getting a coffee break"
+    return "Getting a coffee break";
   }
 
   workDirectorTasks(): string {
-      return "Getting to director tasks"
+    return "Getting to director tasks";
   }
 }
 
 class Teacher implements TeacherInterface {
   workFromHome(): string {
-      return "Cannot work from home"
+    return "Cannot work from home";
   }
 
   getCoffeeBreak(): string {
-      return "Cannot have a break"
+    return "Cannot have a break";
   }
 
   workTeacherTasks(): string {
-      return "Getting to work"
+    return "Getting to work";
   }
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === "number" && salary <= 500) {
-    return new Teacher()
+    return new Teacher();
   } else {
-    return new Director()
+    return new Director();
+  }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    employee.workDirectorTasks();
+  } else {
+    employee.workTeacherTasks();
+  }
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else {
+    return "Teaching History";
   }
 }
